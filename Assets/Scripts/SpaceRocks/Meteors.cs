4dd04b4502538c7118ce4e1f2d868769467e1faa
unknown <shohan4556@gmail.com>
2016-09-27 23:00:56 +0600

    Meteor health code

--- Assets/Scripts/SpaceRocks/Meteors.cs
+++ Assets/Scripts/SpaceRocks/Meteors.cs
@@ -4,31 +4,47 @@ using System.Collections;
 public class Meteors : MonoBehaviour {
 
     public float smooth;
+    public int health;
+
+    [SerializeField]
+    private int damage = 5; // player bullet hit damage
+
+    private int initialHealth;
     private SpriteRenderer _sprite;
-    private bool isHit = false;
-	// Use this for initialization
+
+    // Use this for initialization
 	void Start () {
+        initialHealth = health;
+
         _sprite = GetComponent<SpriteRenderer>();
 	}
 	
     void Update(){
         transform.Rotate(0f, 0f ,smooth );
-     
+
+       // Debug.Log(health);
     }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Bullet"){
             _sprite.color = new Color(255, 0, 0, 255); // when get hit 
-            isHit = true;
             other.gameObject.SetActive(false);
 
-            StartCoroutine(backToColor(0.5f));
+            if (health > 0)
+            {
+                giveMeteorDamage(damage);
+                StartCoroutine(backToColor(0.3f));
+            }
+            else{
+                // health is zero kill meteor
+                gameObject.SetActive(false);
+                health = initialHealth; 
+            }
+                
         }
+            
+    } // end method 
 
-
-           
-        //Debug.Log(other.name);
-    }
 
     IEnumerator backToColor(float secs){
         yield return new WaitForSeconds(secs);
@@ -36,13 +52,10 @@ public class Meteors : MonoBehaviour {
         _sprite.color = new Color(255,255,255,255);
 
     }
-
-    void OnTriggerStay2D(Collider2D col){
-        //Debug.Log(col.tag);
-    }
-
-    public static void getMeteorDamage(int num){
         
+
+    public void giveMeteorDamage(int num){
+        health -= damage;
     }
 
 }
