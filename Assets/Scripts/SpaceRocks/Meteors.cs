f034bfa08c5ae87adc5a5e9888b125dc59c386d1
unknown <shohan4556@gmail.com>
2016-09-27 20:07:44 +0600

    ** meteor color changed when getHit by player bullet ** Prefabs all the meteor gameobject

--- Assets/Scripts/SpaceRocks/Meteors.cs
+++ Assets/Scripts/SpaceRocks/Meteors.cs
@@ -4,23 +4,45 @@ using System.Collections;
 public class Meteors : MonoBehaviour {
 
     public float smooth;
-
+    private SpriteRenderer _sprite;
+    private bool isHit = false;
 	// Use this for initialization
 	void Start () {
-	
+        _sprite = GetComponent<SpriteRenderer>();
 	}
 	
     void Update(){
         transform.Rotate(0f, 0f ,smooth );
-        //iTween.RotateUpdate(this.gameObject,new Vector3(0f,0f,10f),Time.deltaTime * smooth);
+     
     }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Bullet"){
+            _sprite.color = new Color(255, 0, 0, 255); // when get hit 
+            isHit = true;
             other.gameObject.SetActive(false);
+
+            StartCoroutine(backToColor(0.5f));
         }
+
+
+           
         //Debug.Log(other.name);
     }
 
+    IEnumerator backToColor(float secs){
+        yield return new WaitForSeconds(secs);
+         // back to initial color 
+        _sprite.color = new Color(255,255,255,255);
+
+    }
+
+    void OnTriggerStay2D(Collider2D col){
+        //Debug.Log(col.tag);
+    }
+
+    public static void getMeteorDamage(int num){
+        
+    }
 
 }
