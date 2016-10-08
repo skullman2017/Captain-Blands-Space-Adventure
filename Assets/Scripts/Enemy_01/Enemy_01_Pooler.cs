40be6414b8cc406c8b83c1291fff0252ba3e312b
unknown <shohan4556@gmail.com>
2016-10-08 19:54:14 +0600

    Enemy_01 PoolManager and Enemy_01 prefabs

---
+++ Assets/Scripts/Enemy_01/Enemy_01_Pooler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy_01_Pooler : MonoBehaviour {
+
+
+    public int amount;
+    public int secsTowait;
+    [SerializeField]
+    private GameObject prefabs;
+    private List<GameObject> EnemyPool = new List<GameObject>();
+    private int count; 
+    private GameObject enemyHolder;
+
+    // Use this for initialization
+	void Start () {
+        count = 0;
+        enemyHolder = new GameObject("Enemy_01_Holder");
+
+        StartCoroutine(runInstance(secsTowait));
+	}
+	
+    void _instantiate(){
+        for(int i=count ;i<amount;i++){
+            GameObject clone = Instantiate(prefabs, Vector2.up, Quaternion.identity) as GameObject;
+            clone.transform.parent = enemyHolder.transform;
+            clone.SetActive(false);
+
+            EnemyPool.Add(clone);
+            break;
+        }
+    }
+
+
+    IEnumerator runInstance(float _sec){
+        yield return new WaitForSeconds(_sec);
+        // create one
+        if(count<=amount){
+            _instantiate();
+            count++;
+            StartCoroutine(runInstance(secsTowait));
+        }
+
+    }
+
+}
