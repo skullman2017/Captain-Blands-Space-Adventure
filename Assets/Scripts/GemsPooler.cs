a345ae6e5ae0fc3d5b76c0c43fbfe2a7454a46c8
shohan4556 <shohan4556@gmail.com>
2017-01-25 21:24:20 +0600

    GemsPooler added & missing sprite frame added to animation dope sheet.

---
+++ Assets/Scripts/GemsPooler.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// there are objects to pool
+[System.Serializable]
+public class pooledObject {
+    public GameObject Object;
+    public int Amount;
+}
+
+public class GemsPooler : MonoBehaviour {
+
+    public static GemsPooler _Instance;
+    public pooledObject[] _prefabs; // prefabs, store the objects info
+    private List<GameObject>[] thePool; // array of list 
+
+    // used for enemy ID to getEnemyBullets
+    public enum Enemies {
+        gems_01
+    };
+
+
+    [Range(2, 100)]
+    public int secsTowait;
+
+    void Awake() {
+        _Instance = this;
+    }
+
+    // Use this for initialization
+    void Start() {
+        Invoke("createPool", secsTowait);
+
+        // createPool();
+    }
+
+    void createPool() {
+        GameObject clone;
+        // created an array of type list<GameObjetc>
+        thePool = new List<GameObject>[_prefabs.Length];
+
+        for (int count = 0; count < _prefabs.Length; count++) {
+            // here created list in each position of array
+            thePool[count] = new List<GameObject>();
+
+            // now add gameobjects to the list 
+            for (int i = 0; i < _prefabs[count].Amount; i++) {
+                clone = Instantiate(_prefabs[count].Object, Vector2.zero, Quaternion.identity) as GameObject;
+                clone.SetActive(false);
+                clone.transform.parent = this.transform;
+                thePool[count].Add(clone);
+
+            }
+        }
+
+    } // end method 
+
+
+    // give id 
+    // pass param like 
+    /*
+	getBullet((int)Enemies.Enemy_01);
+	*/
+    public GameObject getGems(int id) {
+
+        for (int count = 0; count < thePool[id].Count; count++) {
+            if (thePool[id][count].activeInHierarchy == false) {
+                return thePool[id][count];
+            }
+        }
+
+        // if dont return anything then create a object and add to pool
+        GameObject clone = Instantiate(_prefabs[id].Object, Vector2.zero, Quaternion.identity) as GameObject;
+        clone.SetActive(false);
+        clone.transform.parent = this.transform;
+        thePool[id].Add(clone);
+
+        return clone;
+    }
+
+}
